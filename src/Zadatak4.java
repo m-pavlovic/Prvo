d1d446b15f872cbbb8876655d970435d06c1572d
m-pavlovic <marija.pavlovic19@gmail.com>
2021-03-09 12:59:45 +0100

    4_4 zad u prvim vj

--- src/Zadatak4.java
+++ src/Zadatak4.java
@@ -8,18 +8,19 @@ public class Zadatak4 {
         while (flag) {
             System.out.println("Unesite neki proizvoljni niz: ");
             Scanner sc = new Scanner(System.in);
-            String str = sc.next();
+            String str = sc.nextLine();
             System.out.println("Unijeli ste: " + str);
-            boolean sflag = true;
-                while (sflag) {
+            boolean flag_s = true;
+                while (flag_s) {
                     System.out.println("Ako zelite ponoviti unos pritisnite: y ");
-                    Scanner sc1 = new Scanner(System.in);
-                    String str1 = sc1.next();
-                    if (!str1.equals("y")) {
+                    Scanner sc_f = new Scanner(System.in);
+                    String str_f = sc_f.next();
+                    if (!str_f.equals("y")) {
                         System.out.println("Izabrali ste izlaz iz programa!");
                         flag = false;
                     }
-                    sflag = false;
+                    flag_s = false;
+                    sc_f.reset();
                 }
 
             sc.reset();
